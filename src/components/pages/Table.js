3eda907b5e362a8ce923e767367907b55e435303
reirysson <reiryssonjp@gmail.com>
2022-05-22 16:36:46 -0300

    css iniciado da tabela e mostragem da mesma

--- src/components/pages/Table.js
+++ src/components/pages/Table.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import styles from './Table.module.css'
 
 function Table() {
     const [alimentos, setAlimentos] = useState([])
@@ -12,40 +13,53 @@ function Table() {
         })
             .then((resp) => resp.json())
             .then((data) => {
-                console.log(data)
+                //console.log(data)
                 setAlimentos(data)
             })
             .catch(err => console.log(err))
     }, [])
 
+    function mostrarTabela(){
+        document.getElementById('cafe_manha').style.display = '';
+    }
+
     return (
-        <div>
-            <table>
-                <tr>
-                    <th>Alimento</th>
-                    <th>Quantidade</th>
-                    <th>Energia</th>
-                </tr>
-                <tr>
-                    <td>
-                    {alimentos.length > 0 &&
-                    alimentos.map((alimento) => (
-                    <p>{alimento.nome}</p>
-                    ))}
-                    </td>
-                    <td>
-                    {alimentos.length > 0 &&
-                    alimentos.map((alimento) => (
-                    <p>{alimento.quantidade}</p>
-                    ))} 
-                    </td>
-                    <td>
-                    {alimentos.length > 0 &&
-                    alimentos.map((alimento) => (
-                    <p>{alimento.calorias}</p>
-                    ))}   
-                    </td>
-                </tr>
+        <div className={styles.container}>
+            <input type="submit" id="enviar" value="Café da manhã" onClick={(mostrarTabela)}></input>
+            <a href="/">Lanche da manhã</a>
+            <a href="/">Almoço</a>
+            <a href="/">Lanche da tarde</a>
+            <a href="/">Jantar</a>
+            <a href="/">Ceia</a>
+
+            <table id="cafe_manha">
+                <tbody>
+                    <tr>
+                        <th>Alimento</th>
+                        <th>Quantidade</th>
+                        <th>Energia</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            {alimentos.length > 0 &&
+                            alimentos.map((alimento) => (
+                            <p>{alimento.nome}</p>
+                            ))}
+                        </td>
+                        <td>
+                            {alimentos.length > 0 &&
+                            alimentos.map((alimento) => (
+                            <p>{alimento.quantidade}</p>
+                            ))} 
+                        </td>
+                        <td>
+                            {alimentos.length > 0 &&
+                            alimentos.map((alimento) => (
+                            <p>{alimento.calorias}</p>
+                            ))}   
+                        </td>
+                    </tr>
+                </tbody>  
             </table>
 
             
