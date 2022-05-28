da98909ff02f1211cb3e8de911cabef4b0337ef7
reirysson <reiryssonjp@gmail.com>
2022-05-27 22:27:21 -0300

    iniciando o popup da meta

--- src/components/pages/Meta.js
+++ src/components/pages/Meta.js
@@ -2,7 +2,10 @@ import styles from './Meta.module.css'
 
 function Meta(){
     return (
-        <div><p>Aqui será descrito as metas no popup</p></div>
+        <div>
+            <p>Aqui será descrito as metas no popup</p>
+            <p>Aqui tera o popup</p>
+        </div>
     )
 }
 
