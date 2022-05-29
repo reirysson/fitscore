e8f919d195c8544eac980a6b695e6576da4ce20d
reirysson <reiryssonjp@gmail.com>
2022-05-28 22:29:27 -0300

    ajuste no footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,26 +1,14 @@
-import { Link } from 'react-router-dom'
-import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
-
-import Container from './Container'
-
 import styles from './Footer.module.css'
 
 
 function Footer() {
     return (<footer className={styles.footer}>
-        <Container>
-        <div>
+        <div >
             <h1> &copy; FitScore </h1>
-                <nav className={styles.footer_menu}>
-                    <Link to="/speak">Fale conosco</Link>
-                    <Link to="/company">Quem somos</Link>
-                    <Link to="/about">Sobre</Link>
-                </nav>
         </div>
         <div>
-            <p className={styles.down}>FitScore. Todos os direitos reservados</p>
+            <p>FitScore. Todos os direitos reservados</p>
         </div>
-        </Container>
     </footer>
     )
 }
