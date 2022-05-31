7ff258d5193f8175e7b0c40e2b53427d1292f572
reirysson <reiryssonjp@gmail.com>
2022-05-31 08:26:59 -0300

    adicionando a tabela ao checklist

--- src/components/layout/Checklist.js
+++ src/components/layout/Checklist.js
@@ -5,6 +5,8 @@ import { useEffect,useState } from 'react'
 function Checklist({ alimentoData }) {
     const [horarios, setHorarios] = useState([])
     const [alimento, setAlimento] = useState(alimentoData || {})
+    const [alimentos, setAlimentos] = useState([])
+    const [alimentosFiltrados, setAlimentosFiltrados] = useState([])
     
     useEffect(() => {
         fetch('http://localhost:5000/horarios', {
@@ -20,6 +22,22 @@ function Checklist({ alimentoData }) {
             .catch(err => console.log(err))
     }, [])
 
+    useEffect(() => {
+        fetch('http://localhost:5000/alimentos', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                //console.log(data)
+                setAlimentos(data)
+                setAlimentosFiltrados(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     function handleHorario(e) {
         setAlimento({
             ...alimento,
@@ -28,9 +46,32 @@ function Checklist({ alimentoData }) {
         console.log(alimento)
     }
 
+    function filtrarTipo(tipo){
+        if (tipo === "Todos")
+            setAlimentosFiltrados(alimentos)
+        else 
+            setAlimentosFiltrados(alimentos.filter(a => a.tipo === tipo))
+    }
+
     return(
         <div>
-            <Select name="tipo_id" options={horarios} handleOnChange={handleHorario} value={alimento.tipo ? alimento.tipo : ''} />
+            <div>
+                <Select name="tipo_id" options={horarios} handleOnChange={handleHorario} value={alimento.tipo ? alimento.tipo : ''} />
+            </div>
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Nome do alimento</th>
+                        </tr>
+                        {alimentosFiltrados.map((alimento) => (
+                            <tr key={alimento.id}>
+                                <td>{alimento.nome}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
