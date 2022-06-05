baae87f20f852b61f5ccdfa322811da54b355295
reirysson <reiryssonjp@gmail.com>
2022-06-04 23:48:02 -0300

    iniciando o css da pag contato

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,19 @@
+import styles from './Contact.module.css'
+import Input from '../layout/Input'
+
 function Contact() {
     return (
         <div>
-            <p>Aqui é a página de contato</p>
+            <h1>Entre em contato</h1>
+
+            <p>Insira seu e-mail e digite sua sugestão para entrar em contato conosco</p>
+
+            <Input type="text" text="E-mail: *" name="email" placeholder="Ex.: meuemail@gmail.com"/>
+            <Input type="text" text="Envie sua sugestão: *" name="sugestao" placeholder="Ex.: sua sugestão"/>
+
+            <div className={styles.btn_sugestion}>
+                <button>Enviar sugestão</button>
+            </div>
         </div>
     )
 }
