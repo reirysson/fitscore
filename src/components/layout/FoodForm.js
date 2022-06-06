d3066705cb54003492bf7f0488203b750144a08b
reirysson <reiryssonjp@gmail.com>
2022-06-06 16:31:42 -0300

    ajuste no pop-up do menu

--- src/components/layout/FoodForm.js
+++ src/components/layout/FoodForm.js
@@ -40,9 +40,9 @@ function FoodForm({handleSubmit, alimentoData, acionar}) {
                 <p>Preencha as informações a baixo para adicionar um alimento ao cardápio</p>
                 <div className={styles.input_container}>
                     <Input type="text" text="Nome do alimento:" name="nome" placeholder="Buscar nome do alimento" handleOnChange={handleChange} value={alimento.nome ? alimento.nome : ''}/>
-                    <Select name="horario_id" text="Selecione o horário" options={horarios} handleOnChange={handleHorario} value={alimento.tipo ? alimento.tipo : ''}/>
                     <Input type="number" text="Quantidade (em g):" name="quantidade" placeholder="Ex.: 100" handleOnChange={handleChange} value={alimento.quantidade ? alimento.quantidade : ''}/>
                     <Input type="number" text="Calorias (Kcal):" name="calorias" placeholder="Ex.: 350" handleOnChange={handleChange} value={alimento.calorias ? alimento.calorias : ''}/>
+                    <Select name="horario_id" text="Selecione o horário" options={horarios} handleOnChange={handleHorario} value={alimento.tipo ? alimento.tipo : ''}/>
                 </div> 
                 <button type="submit" className={styles.save_btn} onClick={() => acionar=(false)}>Salvar alimento</button>
             </form>
